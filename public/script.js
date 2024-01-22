a2186e0766decfbf1c02d0970c64b27525f8e552
MherKaroyan521 <mher.karoyan@tumo.org>
2024-01-22 21:28:08 +0400

    i hate everything

--- public/script.js
+++ public/script.js
@@ -5,11 +5,13 @@ submit.addEventListener("click", values)
 function values(){
     let nameinput = document.getElementById("name").value;
     let passwordinput = document.getElementById("password").value;
+    let emailinput = document.getElementById("email").value;
+
     fetch('/submit', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({name:nameinput, password:passwordinput})
+        body: JSON.stringify({name:nameinput, password:passwordinput, email:emailinput})
       })
 }
--- server.js
+++ server.js
@@ -1,47 +1,68 @@
-// const express = require('express'); 
-// const bodyParser = require("body-parser");
-// var path = require("path");
-// const app = express();
-// const port = 3000;
-
-// app.use(bodyParser.urlencoded({ extended: true}))
-// app.use(bodyParser.json())
-
-// app.use(express.static("public"));
-
-// app.get('/', function(req,res){
-//     res.sendFile(path.join(__dirname,'./public/forma.html'));
-// }); 
-
-// app.post('/submit', function(req,res){
-//     let name = req.body.name;
-//     let password = req.body.password;
-//     console.log(name, password)
-//     res.redirect("/")
-// }); 
-
-// app.listen(port, () => {
-//    console.log(`Server is running at http://localhost:${port}`);
-// });
-
+const express = require('express'); 
+const bodyParser = require("body-parser");
+var path = require("path");
+const app = express();
 const mongoose = require('mongoose');
+const connectionString = 'mongodb+srv://mherkaroyan:M2h0e0r9^@cluster0.crtiwy0.mongodb.net/sample_mflix';
+
+
+const bcrypt = require('bcrypt');
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true}))
+app.use(bodyParser.json())
+
+app.use(express.static("public"));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname,'./public/forma.html'));
+}); 
+
+
+app.post('/submit', async function(req,res){
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password;
+    console.log(name, password)
+
+    mongoose.connect(connectionString, { useUnifiedTopology: true });
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+
+    db.once('open', async () => {
+        console.log('Connected to MongoDB!');
+        try {
+    
+            let result = await mongoose.connection.db.collection('users').insertOne({
+                name: name,
+                email: email,
+                password: password
+            })
+
+
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+    
+        // You can add additional code here for testing or other operations
+        // Make sure to close the connection when you're done
+        mongoose.connection.close();
+    });
+
+
+    res.redirect("/")
+}); 
+
+
+
+app.listen(port, () => {
+   console.log(`Server is running at http://localhost:${port}`);
+});
+
+
 
 // Replace the connection string with your MongoDB connection string
-const connectionString = 'mongodb+srv://mherkaroyan:M2h0e0r9^@cluster0.crtiwy0.mongodb.net/?retryWrites=true&w=majority';
 
-
-// Connect to MongoDB
-mongoose.connect(connectionString, { useUnifiedTopology: true });
-
-// Check the connection
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Connection error:'));
-db.once('open', () => {
-console.log('Connected to MongoDB!');
-db.users.insert( { item: "card", qty: 15 } )
-
-
-// You can add additional code here for testing or other operations
-// Make sure to close the connection when you're done
-mongoose.connection.close();
-});
