7750251915a2fa56980315b8af07f93f7c9eed30
MherKaroyan521 <mher.karoyan@tumo.org>
2024-01-26 21:26:23 +0400

    it wiil end soon

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');
 const bodyParser = require("body-parser");
 var path = require("path");
 const app = express();
@@ -6,17 +6,47 @@ const mongoose = require('mongoose');
 const connectionString = 'mongodb+srv://mherkaroyan:M2h0e0r9^@cluster0.crtiwy0.mongodb.net/sample_mflix';
 
 
-const bcrypt = require('bcrypt');
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(bodyParser.json())
 
 app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname,'./public/forma.html'));
-}); 
+    mongoose.connect(connectionString, { useUnifiedTopology: true });
+
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'Connection error:'));
+
+    db.once('open', async () => {
+        console.log('Connected to MongoDB!');
+
+        try {
+
+            let result = await mongoose.connection.db.collection('theaters').find({'location.address.city': "Bloomington"}).toArray()
+            res.render('../public/forma.ejs', {
+                res: result
+            });
+            console.log(result)
+
+
+
+        } catch (error) {
+            console.error('Error retrieving movies:', error);
+        } finally {
+            mongoose.connection.close();
+        }
+
+        // You can add additional code here for testing or other operations
+        // Make sure to close the connection when you're done
+        mongoose.connection.close();
+    });
+    var tagline = "Sans Bad Time";
+
+
+});
 
 
 app.post('/submit', async function(req,res){
@@ -33,7 +63,7 @@ app.post('/submit', async function(req,res){
     db.once('open', async () => {
         console.log('Connected to MongoDB!');
         try {
-    
+
             let result = await mongoose.connection.db.collection('users').insertOne({
                 name: name,
                 email: email,
@@ -46,7 +76,7 @@ app.post('/submit', async function(req,res){
         } finally {
             mongoose.connection.close();
         }
-    
+
         // You can add additional code here for testing or other operations
         // Make sure to close the connection when you're done
         mongoose.connection.close();
@@ -54,7 +84,7 @@ app.post('/submit', async function(req,res){
 
 
     res.redirect("/")
-}); 
+});
 
 
 
@@ -65,4 +95,3 @@ app.listen(port, () => {
 
 
 // Replace the connection string with your MongoDB connection string
-
